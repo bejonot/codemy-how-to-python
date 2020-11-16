291f3d675cc9a258740cf92c8d5eeeb2a3dbf2b7
bejonot <root@LAPTOP-J9MNB7D0.localdomain>
2020-11-17 05:22:08 +0700

    latihan1-1-.py works.

---
+++ latihan1-1.py
@@ -0,0 +1,7 @@
+# Nama file : latihan1-1.py
+# Tanggal : 17 November 2020
+# Deskripsi : Code Python ini merupakan latihan Problem 1.1 yang bersumber dari Codemy.
+# Tuan Yon sangat senang karena tahun ini mahasiswa ITB angkatan 2019 masuk ke ITB. Karenanya, ia ingin menyambut mahasiswa ITB dengan membuat program yang menerima nama mahasiswa baru dan menuliskan ucapan selamat datang.
+
+Nama_maba = input("Masukkan nama mahasiswa baru : ")
+print("Halo %s, selamat datang di ITB!" % Nama_maba)
