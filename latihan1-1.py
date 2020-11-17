08e9b2c75f40646e67416f0f7c67320f8889a4bb
bejonot <root@LAPTOP-J9MNB7D0.localdomain>
2020-11-17 09:23:43 +0700

    add programmer name comment

--- latihan1-1.py
+++ latihan1-1.py
@@ -1,5 +1,6 @@
 # Nama file : latihan1-1.py
 # Tanggal : 17 November 2020
+# Programmer : bejonot
 # Deskripsi : Code Python ini merupakan latihan Problem 1.1 yang bersumber dari Codemy.
 # Tuan Yon sangat senang karena tahun ini mahasiswa ITB angkatan 2019 masuk ke ITB. Karenanya, ia ingin menyambut mahasiswa ITB dengan membuat program yang menerima nama mahasiswa baru dan menuliskan ucapan selamat datang.
 
